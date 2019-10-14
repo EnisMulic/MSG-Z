3987811b40cfbd590280c7fec72a28b226864dbc
Enis Mulić <enis.s.mulic@gmail.com>
2019-10-14 20:30:54 +0200

    Revert "adding moderator message manipulation functions"
    This reverts commit 8dc7ce927918655b3b6d307f87b3f6adb988e6dd.

--- cogs/utils/misc.py
+++ cogs/utils/misc.py
@@ -1,7 +1,7 @@
 import discord
 
 def getChannelID(client, channelName):
-    for guild in client.guilds:
-        for channel in guild.channels:
-            if channel.name == channelName:
-                return channel.id
+        for guild in client.guilds:
+            for channel in guild.channels:
+                if channel.name == channelName:
+                    return channel.id
