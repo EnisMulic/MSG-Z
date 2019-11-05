37dcc963fafbf39b964ede7398679752203fdee7
Enis Mulić <enis.s.mulic@gmail.com>
2019-11-05 22:58:57 +0100

    fix exceeding login tries

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import discord
 from discord.ext import commands
 
--- cogs/scraper.py
+++ cogs/scraper.py
@@ -19,7 +19,7 @@ class Scraper(commands.Cog):
             data = json.load(json_for_url)
         self.loginUrl = data["DLWMS"]["url"]
 
-        self.send_notification.start()
+        self.get_notifications()
 
 
     def get_value_for_input(self, html, inputName):
@@ -67,86 +67,94 @@ class Scraper(commands.Cog):
 
         return loginData
 
-    def get_notifications(self):
-        with requests.session() as session:
-            try:
-                loginResponse = session.post(self.loginUrl, data = self.get_login())
-                response = BeautifulSoup(loginResponse.text, "html.parser")
+    @tasks.loop(minutes = 1)
+    async def send_notifications(self, session, loginResponse):
+        try:
+            print("Scraping...")
+            
+            contentResponse = session.get('https://fit.ba/student/', cookies = loginResponse.cookies)
+            response = BeautifulSoup(contentResponse.text, "html.parser")
 
-                notificationsList = []
-                news = response.select("ul.newslist")
-                for new in news:
-                    link = 'https://fit.ba/student/' + new.find("a", {"class": "linkButton"}).get("href")
-                    title = new.find("a", {"class": "linkButton"}).text
-                    date = new.find("span", {"id": "lblDatum"}).text[:16]
-                    subject = new.find("span", {"id": "lblPredmet"}).text
-                    author = new.find("a", {"id": "HyperLink9"}).text
-                    content = new.find("div", {"class": "abstract"}).text
+            notificationsList = []
+            news = response.select("ul.newslist")
+            for new in news:
+                link = 'https://fit.ba/student/' + new.find("a", {"class": "linkButton"}).get("href")
+                title = new.find("a", {"class": "linkButton"}).text
+                date = new.find("span", {"id": "lblDatum"}).text[:16]
+                subject = new.find("span", {"id": "lblPredmet"}).text
+                author = new.find("a", {"id": "HyperLink9"}).text
+                content = new.find("div", {"class": "abstract"}).text
 
                     
-                    notificationsList.append(
-                        notifications.DLWMS_Notification(
-                            link, title, date, subject, author, content
-                        )
+                notificationsList.append(
+                    notifications.DLWMS_Notification(
+                        link, title, date, subject, author, content
                     )
-                                
-                return notificationsList
+                )
+
             
-            except Exception as err:
-                print("Error: " + str(err))
+            lastNotificationJson = {}
+        
+            with open(".\\lastNotification.json", "r") as jsonDataFile:
+                lastNotificationJson = json.load(jsonDataFile)
+
+            lastNotification = notifications.DLWMS_Notification(
+                lastNotificationJson["link"],
+                lastNotificationJson["title"],
+                lastNotificationJson["date"],
+                lastNotificationJson["subject"],
+                lastNotificationJson["author"],
+                lastNotificationJson["content"]
+            )
+            lastSent = lastNotification
+            notificationsList.reverse()
+            for notification in notificationsList or []:
+                if notification > lastNotification:
+                    with open(".\\config.json") as jsonSubjectChannel:
+                        data = json.load(jsonSubjectChannel)
+
+                    try:
+                        channelName = data["DLWMS"]["subjects"][notification.subject]
+                    except:
+                        channelName = "generalna-diskusija-3"
+                        pass
+
+                        
+                    channel = self.client.get_channel(misc.getChannelID(self.client, channelName))
+                    if channel is not None:
+                        await channel.send(embed = notification.getEmbed())
+                    lastSent = notification
+
+
+            if lastSent != lastNotification:
+                with open(".\\lastNotification.json", "w") as jsonDataFile:
+                    json.dump(lastSent.__dict__, jsonDataFile, indent = 4)
+            
+        except Exception as err:
+            print("Error: " + str(err))        
+            
 
     
 
-    @tasks.loop(minutes = 1)
-    async def send_notification(self):
-        print("Scraping...")
-        notificationsList = self.get_notifications()
-
-        lastNotificationJson = {}
+    
+    def get_notifications(self):
+        try:
+            with requests.session() as session:
+                loginResponse = session.post(self.loginUrl, data = self.get_login())
+            self.send_notifications.start(session, loginResponse)
         
-        with open(".\\lastNotification.json", "r") as jsonDataFile:
-            lastNotificationJson = json.load(jsonDataFile)
-
-        lastNotification = notifications.DLWMS_Notification(
-            lastNotificationJson["link"],
-            lastNotificationJson["title"],
-            lastNotificationJson["date"],
-            lastNotificationJson["subject"],
-            lastNotificationJson["author"],
-            lastNotificationJson["content"]
-        )
-
-        lastSent = lastNotification
-
-        notificationsList.reverse()
-        for notification in notificationsList or []:
-            if notification > lastNotification:
-                with open(".\\config.json") as jsonSubjectChannel:
-                    data = json.load(jsonSubjectChannel)
-
-                try:
-                    channelName = data["DLWMS"]["subjects"][notification.subject]
-                except:
-                    channelName = "generalna-diskusija-3"
-                    pass
-
-                
-                channel = self.client.get_channel(misc.getChannelID(self.client, channelName))
-                if channel is not None:
-                    await channel.send(embed = notification.getEmbed())
-                lastSent = notification
-
-
-        if lastSent != lastNotification:
-            with open(".\\lastNotification.json", "w") as jsonDataFile:
-                json.dump(lastSent.__dict__, jsonDataFile, indent = 4)
+        except Exception as err:
+            print(str(err))
+            self.get_notifications()
+        
+            
 
 
     def cog_unload(self):
-        self.send_notification.cancel()
+        self.send_notifications.cancel()
 
-    @send_notification.before_loop
-    async def before_send_notification(self):
+    @send_notifications.before_loop
+    async def before_send_notifications(self):
         print('Scraper: Waiting...')
         await self.client.wait_until_ready()
 
