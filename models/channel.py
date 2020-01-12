0731e2fd9b9d76ac3279b6d7384c302a2f29eeab
Enis Mulić <enis.s.mulic@gmail.com>
2020-01-12 08:50:03 +0100

    [Add] Channels to db

---
+++ models/channel.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, BigInteger, Table, ForeignKey
+from sqlalchemy.orm import relationship, backref
+
+from models.base import Base
+
+
+class Channel(Base):
+    __tablename__ = 'Channels'
+
+    Id = Column(BigInteger, primary_key = True)
+    Name = Column(String(32), nullable = False)
+    
+
+    def __init__(self, Id, Name):
+        self.Id = Id
+        self.Name = Name
