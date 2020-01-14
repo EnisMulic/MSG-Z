3bdcc12537588167c626aab30930e6a10c43c2fe
Enis Mulić <enis.s.mulic@gmail.com>
2020-01-14 09:26:45 +0100

    [Update] Tags

--- cogs/tag.py
+++ cogs/tag.py
@@ -8,6 +8,13 @@ from sqlalchemy.exc import SQLAlchemyError
 import models.tag as tg
 from models.user import User
 
+import string
+import random
+import datetime
+
+def generate_id(size = 4, chars = string.ascii_letters + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
 class Tag(commands.Cog):
     def __init__(self, client):
         self.client = client
@@ -25,7 +32,7 @@ class Tag(commands.Cog):
                         .one()
 
 
-                newTag = tg.Tag(tag_name, url, user)
+                newTag = tg.Tag(tag_name, url, datetime.datetime.now(), user)
                 session.add(newTag)
                 session.commit()
 
@@ -76,6 +83,24 @@ class Tag(commands.Cog):
                 print(str(err))
             finally:
                 session.close()
+    
+    @commands.command(aliases=["edit-tag"])
+    @commands.has_any_role('Administrator', 'Moderator')
+    async def edit_tag(self, ctx, id: int, link: str):
+        database = self.client.get_cog('Database')
+        if database is not None:
+            try:
+                session = database.Session()
+                tag = session.query(tg.Tag) \
+                    .filter(tg.Tag.Id == id) \
+                    .one()
+                tag.Link = link
+                session.commit()
+
+            except SQLAlchemyError as err:
+                print(str(err))
+            finally:
+                session.close()
 
     @commands.command(aliases=["tag"])
     async def get_tag(self, ctx, *, search: str):
@@ -88,7 +113,7 @@ class Tag(commands.Cog):
 
             description = '\n'
             for tag in tags:
-                description += f"\n{tag.Id}. [{tag.Name}]({tag.Link})"
+                description += f"\n:bookmark: | [{tag.Name}]({tag.Link})"
                 tag.Count += 1
                               
             embed = discord.Embed(
@@ -100,6 +125,58 @@ class Tag(commands.Cog):
             await ctx.send(embed = embed)
             session.commit()
             session.close()
+
+    @commands.command(aliases=["tag-info"])
+    async def tag_info(self, ctx, *, search: str):
+        database = self.client.get_cog('Database')
+        if database is not None:
+            session = database.Session()
+            try:
+                tag = session.query(tg.Tag) \
+                    .filter(tg.Tag.Name.ilike(f"%{search}%")) \
+                    .one()
+
+                embed = discord.Embed(
+                    title = ":bookmark: Tag info",
+                    colour = discord.Colour.blurple().value
+                ) 
+
+                embed.add_field(
+                    name = "Id",
+                    value = str(tag.Id),
+                    inline = True
+                )
+
+                embed.add_field(
+                    name = "Name",
+                    value = f"[{tag.Name}]({tag.Link})",
+                    inline = True
+                )
+
+                embed.add_field(
+                    name = "Count",
+                    value = str(tag.Count),
+                    inline = False
+                )
+
+                embed.add_field(
+                    name = "Created at",
+                    value = tag.Created.strftime("%d.%m.%Y %H:%M:%S"),
+                    inline = True
+                )
+
+                embed.add_field(
+                    name = "Owner",
+                    value = "<@" + str(tag.User.UserId) + ">",
+                    inline = False
+                )
+
+                
+                await ctx.send(embed = embed)
+            except SQLAlchemyError as err:
+                print(str(err))
+            finally:
+                session.close()
 
 def setup(client):
     client.add_cog(Tag(client))
--- models/tag.py
+++ models/tag.py
@@ -3,21 +3,22 @@ from sqlalchemy.orm import relationship, backref
 
 from models.base import Base
 
-
 class Tag(Base):
-    __tablename__ = 'Tag'
+    __tablename__ = 'Tags'
 
     Id = Column(Integer, primary_key = True)
-    Name = Column(String(256), nullable = False, unique = True)
+    Name = Column(String(100), nullable = False, unique = True)
     Link = Column(String(2084), nullable = False, unique = True)
     Count = Column(Integer, default = 0)
+    Created = Column(DateTime)
     
     
     UserId = Column(BigInteger, ForeignKey('Users.UserId'), nullable = False)
     User = relationship('User')
 
-    def __init__(self, Name, Link, User, Count = 0):
+    def __init__(self, Name, Link, Created, User, Count = 0):
         self.Name = Name
         self.Link = Link
+        self.Created = Created
         self.User = User
         self.Count = Count
