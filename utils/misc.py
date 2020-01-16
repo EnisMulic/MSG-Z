deba837365099860e531052df556d4a1b499618f
Enis Mulić <enis.s.mulic@gmail.com>
2020-01-16 10:19:23 +0100

    [Add] GetMember

--- utils/misc.py
+++ utils/misc.py
@@ -5,3 +5,9 @@ def getChannelID(client, channelName):
         for channel in guild.channels:
             if channel.name == channelName:
                 return channel.id
+
+def getMember(client, member_id):
+    for guild in client.guilds:
+        for member in guild.members:
+            if member.id == member_id:
+                return member
