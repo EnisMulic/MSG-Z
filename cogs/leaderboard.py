a70759e5bb3786b4ebf53e80c0f31d4d7b823785
Enis Mulić <enis.s.mulic@gmail.com>
2020-02-02 23:04:24 +0100

    function

--- cogs/leaderboard.py
+++ cogs/leaderboard.py
@@ -13,6 +13,7 @@ from models.role import Role
 from models.user import User
 from models.channel import Channel
 from models.youtube import Youtube
+from models.users_channels import UsersChannelsActivity
 
 import datetime
 
@@ -22,6 +23,7 @@ from utils import misc
 class Leaderboard(commands.Cog):
     def __init__(self, client):
         self.client = client
+        self.message_point_multiplier = 0.25
 
     @commands.command()
     @commands.has_any_role('Administrator') 
@@ -30,9 +32,111 @@ class Leaderboard(commands.Cog):
         messages = await channel.history(oldest_first = True, limit = None).flatten()
         for message in messages:
             f.write("Author: " + message.author.name + " | " + str(message.author.id) + " | " 
-               + str(message.created_at)
+               + str(message.created_at) + " | " + str(len(message.content))
                + "\n---------------------------------------\n")
         f.close()
+
+    @commands.command(aliases = ["Populate"])
+    @commands.has_any_role('Administrator', 'Moderator')
+    async def populate(self, ctx):
+        session = base.Session()
+        # channels_in_db = session.query(Channel)
+        # users_in_db = session.query(User)
+        for guild in self.client.guilds:
+            for channel in guild.channels:
+                if session.query(Channel).filter(Channel.Id == channel.id).one_or_none() is not None:
+                    messages = await channel.history(oldest_first = True, limit = None).flatten()
+                    for message in messages:
+                        if session.query(User).filter(User.UserId == message.author.id).one_or_none() is not None:
+                            usersAcctivity = session.query(UsersChannelsActivity) \
+                                .filter(UsersChannelsActivity.UserId == message.author.id and \
+                                        UsersChannelsActivity.ChannelId == channel.id).one_or_none()
+                            if usersAcctivity is not None:
+                                usersAcctivity.NumberOfMessages = usersAcctivity.NumberOfMessages + 1
+                                usersAcctivity.LengthOfMessages = usersAcctivity.LengthOfMessages + len(message.content)
+                                usersAcctivity.Points = usersAcctivity.Points + int(len(message.content) * self.message_point_multiplier)
+                                
+                                session.commit()
+                            else:
+                                usersAcctivity = UsersChannelsActivity(
+                                    UserId = int(message.author.id), 
+                                    ChannelId = int(channel.id), 
+                                    NumberOfMessages = 1, 
+                                    LengthOfMessages = int(len(message.content)),
+                                    Points = int(len(message.content) * self.message_point_multiplier)
+                                )
+
+                                session.add(usersAcctivity)
+                                session.commit()
+                        else:
+                            try:
+                                f = open(f"./Archive/{message.author.name}.txt", "a", encoding="utf-8")
+                                f.write(channel.name + " | " + str(len(message.content)) + " = " + 
+                                    str(len(message.content) * self.message_point_multiplier)
+                                    + "\n---------------------------------------\n")
+                                f.close()
+                            except:
+                                print("Name is wrong")
+
+                # try:
+                #     dbChannel = session.query(Channel).filter(channel.id == Channel.Id).one()
+                #     print("Found channel " + channel.name)
+                #     messages = await channel.history(oldest_first = True, limit = None).flatten()
+                #     for message in messages:
+                        
+                #         try:
+                #             dbUser = session.query(User).filter(message.author.id == User.UserId).one()
+                #             print("DbUserID: " + dbUser.UserId)
+                #             print("------------------------------------")
+                #             try:
+                            
+                #                 print("Found user " + dbUser.Name)
+                #                 dbUserActivity = None
+                #                 dbUserActivity = session.query(UsersChannelsActivity) \
+                #                     .filter(dbUser.UserId == User.UserId and channel.id == Channel.Id).one()
+                                
+                                
+                #                 print("User has activity")
+                #                 print(str(dbUserActivity.UserId))
+                #                 dbUserActivity.NumberOfMessages = dbUserActivity.NumberOfMessages + 1
+                #                 dbUserActivity.LengthOfMessages = dbUserActivity.LengthOfMessages + len(message.content)
+                #                 dbUserActivity.Points = dbUserActivity.Points + int(len(message.content) * self.message_point_multiplier)
+                            
+                #                 session.commit()
+                #                 # else:
+                #                 #     print("User had no activity")
+                #                 #     dbUserActivity = UsersChannelsActivity(
+                #                 #         message.author.id, 
+                #                 #         channel.id, 
+                #                 #         1, 
+                #                 #         len(message.content),
+                #                 #         len(message.content) * self.message_point_multiplier
+                #                 #     )
+                #                 #     print(dbUserActivity.UserId)
+                #                 #     session.add(dbUserActivity)
+                #                 #     session.commit()
+                #             except Exception as err:
+                #                 print(str(err))
+                #                 newUserChannelActivity = UsersChannelsActivity(
+                #                     UserId = int(message.author.id), 
+                #                     ChannelId = int(channel.id), 
+                #                     NumberOfMessages = 1, 
+                #                     LengthOfMessages = int(len(message.content)),
+                #                     Points = int(len(message.content) * self.message_point_multiplier)
+                #                 )
+
+                #                 session.add(newUserChannelActivity)
+                #                 session.commit()
+                #         except:
+                #             f = open(f"./Archive/{message.author.name}.txt", "a", encoding="utf-8")
+                #             f.write(channel.name + " | " + str(len(message.content)) + " = " + 
+                #                 str(len(message.content) * self.message_point_multiplier)
+                #                 + "\n---------------------------------------\n")
+                #             f.close()
+                        
+                # except Exception as err:
+                #     print(str(err))
+
 
     @commands.command(aliases = ["Top10"])
     @commands.has_any_role('Administrator', 'Moderator')
