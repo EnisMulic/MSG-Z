9e92206e96680eed0d625e358838cdb29cb62cbb
Enis Mulić <enis.s.mulic@gmail.com>
2020-02-22 20:48:07 +0100

    [Update] database for rankup.py

--- models/role.py
+++ models/role.py
@@ -14,6 +14,7 @@ class Role(Base):
 
     RoleId = Column(BigInteger, primary_key = True)
     Name = Column(String(32), nullable = False)
+    HigherRole = Column(BigInteger, nullable = True)
 
     #Users = relationship('User')
 
