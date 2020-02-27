34fb3c6edb5b86bc547fd8812e1d2de158978c57
Enis Mulić <enis.s.mulic@gmail.com>
2020-02-27 11:49:32 +0100

    [Add] binary tree node and binary tree stuctures

--- cogs/rankup.py
+++ cogs/rankup.py
@@ -8,6 +8,53 @@ from models.user import User
 
 from utils import misc
 
+class Node:
+    def __init__(self, value):
+        self.left = None
+        self.right = None
+        self.value = value
+
+class Tree:
+    def __init__(self):
+        self.root = None
+
+    def GetRoot(self):
+        return self.root
+
+    def add(self, val):
+        if self.root is None:
+            self.root = Node(val)
+        else:
+            self._add(val, self.root)
+    
+    def _add(self, value, node):
+        if value < node.value:
+            if node.left is not None:
+                self._add(value, node.left)
+            else:
+                node.left = Node(value)
+        else:
+            if node.right is not None:
+                self._add(value, node.right)
+            else:
+                node.right = Node(value)
+    
+    def find(self, value):
+        if self.root is not None:
+            self._find(value, self.root)
+        else:
+            return None
+
+    def _find(self, value, node):
+        if value == node.value:
+            return node
+        elif value < node.value and node.left is not None:
+            return self._find(value, node.left)
+        elif value > node.value and node.right is not None:
+            return self._find(value, node.right)
+        else:
+            return None
+
 class Rankup(commands.Cog):
     def __init__(self, client):
         self.client = client
@@ -17,6 +64,9 @@ class Rankup(commands.Cog):
     def SetRole(self, roleName):
         return misc.getRoleByName(self.client, roleName)
 
+    def SetValue(self, role: Role):
+        pass
+    
     @commands.command()
     @commands.has_any_role('Administrator') 
     async def connect(self, ctx, parentRole: discord.Role, childRole: discord.Role):
@@ -108,6 +158,12 @@ class Rankup(commands.Cog):
     @commands.has_any_role('Administrator') 
     async def obnova(self, ctx):
         await ctx.author.add_roles(self.role)
+        # embed
+
+    @commands.command()
+    @commands.has_any_role('Administrator') 
+    async def kolizija(self, ctx):
+        pass
         # embed
 
     @commands.command()
