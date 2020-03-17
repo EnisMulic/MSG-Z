12b71e19cfc9eb2d9ca0457769a6ccec2e7b3005
Enis Mulić <enis.s.mulic@gmail.com>
2020-03-17 16:12:14 +0100

    [Add] raw data logging

--- utils/logger.py
+++ utils/logger.py
@@ -5,4 +5,8 @@ from utils import misc
 async def LogAction(client, embed):
     channel = client.get_channel(misc.getChannelID(client, 'logger'))
     await channel.send(embed = embed)
+
+async def LogActionRaw(client, content, files):
+    channel = client.get_channel(misc.getChannelID(client, 'logger'))
+    await channel.send(content = content, files = files)
     
