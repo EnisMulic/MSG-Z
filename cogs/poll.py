89a2b05c65d124a8f1b482a40bb918ade00dacb7
PancakeAlchemist <enis.s.mulic@gmail.com>
2020-04-09 09:52:25 +0200

    Add updating poll on edits

--- cogs/poll.py
+++ cogs/poll.py
@@ -27,7 +27,7 @@ class Poll(commands.Cog):
 
     @commands.command(aliases=["add-option"], description = "Add option to a poll")
     @commands.has_any_role('Administrator', 'Moderator')
-    async def add_option(self, ctx, name, text, icon):
+    async def add_option(self, ctx, name, icon, text):
         name = name.strip('\"')
         text = text.strip('\"')
 
@@ -36,15 +36,17 @@ class Poll(commands.Cog):
             try:
                 session = database.Session()
 
-                search_poll = session.query(poll.Poll) \
+                poll_search = session.query(poll.Poll) \
                     .filter(poll.Poll.Name == name) \
                     .one()
 
-                if search_poll is not None:
-                    new_poll_option = poll_option.PollOption(icon, text, search_poll)
+                if poll_search is not None:
+                    new_poll_option = poll_option.PollOption(icon, text, poll_search)
                     session.add(new_poll_option)
                     session.commit()
 
+                    if poll_search.MessageId is not None and poll_search.ChannelId is not None:
+                        await self.update_poll(name, session, poll_search.ChannelId, poll_search.MessageId)
             except Exception as err:
                 await ctx.send(str(err))
             finally:
@@ -73,8 +75,14 @@ class Poll(commands.Cog):
                     session.commit()
 
                     await ctx.send(icon + " removed")
+
+                    if poll_search.MessageId is not None and poll_search.ChannelId is not None:
+                        await self.update_poll(name, session, poll_search.ChannelId, poll_search.MessageId)
+
             except Exception as err:
                 await ctx.send(str(err))
+            finally:
+                session.close()
 
     @commands.command(aliases=["edit-icon"], description = "Edit option emoji in a poll")
     @commands.has_any_role('Administrator', 'Moderator')
@@ -97,8 +105,15 @@ class Poll(commands.Cog):
 
                     poll_option_search.Icon = new_icon
                     session.commit()
+
+                    
+                    if poll_search.MessageId is not None and poll_search.ChannelId is not None:
+                        await self.update_poll(name, session, poll_search.ChannelId, poll_search.MessageId)
+
             except Exception as err:
                 await ctx.send(str(err))
+            finally:
+                session.close()
 
     @commands.command(aliases=["edit-option"], description = "Edit option in a poll")
     @commands.has_any_role('Administrator', 'Moderator')
@@ -116,29 +131,78 @@ class Poll(commands.Cog):
                 if poll_search is not None:
                     poll_option_search = session.query(poll_option.PollOption) \
                                     .filter(poll.Poll.PollId == poll_search.PollId) \
-                                    .filter(poll_option.PollOption.Icon == old_icon) \
+                                    .filter(poll_option.PollOption.Icon == icon) \
                                     .one()
 
                     poll_option_search.Text = new_text
                     session.commit()
+
+                    if poll_search.MessageId is not None and poll_search.ChannelId is not None:
+                        await self.update_poll(name, session, poll_search.ChannelId, poll_search.MessageId)
+
             except Exception as err:
                 await ctx.send(str(err))
+            finally:
+                session.close()
 
     @commands.command(description = "Edit option emoji in a poll")
     @commands.has_any_role('Administrator', 'Moderator')
     async def poll(self, ctx, channel, *, name):
+        name = name.strip('\"')
         channel_id = int(channel[2:len(channel) - 1])
         message_channel = self.client.get_channel(channel_id)
 
-    #build poll before posting or editing
-    async def build_poll(self, session, name):
-        try:
-            poll_build = session.query(poll.Poll, poll_option.PollOption) \
+        database = self.client.get_cog("Database")
+        if database is not None:
+            session = database.Session()
+            embed = await self.build_poll(session, name)
+            
+            message = await ctx.send(embed = embed)
+
+            poll_search = session.query(poll.Poll) \
                             .filter(poll.Poll.Name == name) \
                             .one()
+            
+            poll_search.ChannelId = channel_id
+            poll_search.MessageId = message.id
+            session.commit()
+            session.close()
+            
 
+    #build poll before posting or editing
+    async def build_poll(self, session, name):
+        name = name.strip('\"')
+        try:
+            poll_search = session.query(poll.Poll) \
+                                .filter(poll.Poll.Name == name) \
+                                .one()
+                
+            if poll_search is not None:
+                poll_option_search = session.query(poll_option.PollOption) \
+                                .filter(poll_option.PollOption.PollId == poll_search.PollId) \
+                                .all()
+
+                options = ""
+                for poll_build_option in poll_option_search:
+                    options += poll_build_option.Icon + " " + poll_build_option.Text + "\n\n"
+
+                embed = discord.Embed(
+                    title = poll_search.Name,
+                    description = options
+                )
+
+                return embed
         except Exception as err:
             print(str(err))
+    
+    async def update_poll(self, name, session, channel_id, message_id):
+        name = name.strip('\"')
+        
+        embed = await self.build_poll(session, name)
+        message_channel = self.client.get_channel(channel_id)
+        message = await message_channel.fetch_message(message_id)
+        
+        await message.edit(embed = embed)
 
 def setup(client):
     client.add_cog(Poll(client))
