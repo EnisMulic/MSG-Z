02b9ec2bc54c89b3bb51e48d3ac95f314cc621ff
PancakeAlchemist <enis.s.mulic@gmail.com>
2020-06-29 18:01:43 +0200

    Update news scraper

--- cogs/fitnews.py
+++ cogs/fitnews.py
@@ -8,6 +8,10 @@ from bs4 import BeautifulSoup
 import re
 from datetime import datetime
 
+from utils import notifications
+from utils import misc
+import json
+
 class FitNews(commands.Cog):
     def __init__(self, client):
         self.client = client
@@ -21,62 +25,62 @@ class FitNews(commands.Cog):
     async def main(self):
         print("Scraping FIT News...")
 
-        async with aiohttp.ClientSession() as session:
-            html = await fetch(session, self.url)
-            response = BeautifulSoup(html, "html.parser")
-            news = response.select("li.media")
+        try:
+            async with aiohttp.ClientSession() as session:
+                html = await self.fetch(session, self.url)
+                response = BeautifulSoup(html, "html.parser")
+                news = response.select("li.media")
             
             notifications_list = []
             for new in news:
                 link = 'https://fit.ba/' + new.find("a", {"class": "cover"}).get("href")
                 meta = new.find("small").text
-                datetime = re.search(r'\d{2}.\d{2}.\d{4}. \d{2}:\d{2}', meta)
+                date = re.search(r'\d{2}.\d{2}.\d{4}', meta)
+                time = re.search(r'\d{2}:\d{2}', meta)
                 author = meta.split() 
 
                 notifications_list.append(
                     notifications.DLWMS_Notification(
-                        link, "", datetime, "", author, ""
+                        link, "", date.group() + " " + time.group(), "", author[1] + " " + author[2], ""
                     )
                 )
 
-            try:
-                last_notification_json = {}
+            last_notification_json = {}
+        
+            with open(".\\last_notification_fit_news.json", "r", encoding="utf-8") as jsonDataFile:
+                last_notification_json = json.load(jsonDataFile)
+
+            last_notification = notifications.DLWMS_Notification(
+                last_notification_json["link"],
+                last_notification_json["title"],
+                last_notification_json["date"],
+                last_notification_json["subject"],
+                last_notification_json["author"],
+                last_notification_json["content"]
+            )
+
+            lastSent = last_notification
+            notifications_list.reverse()
+
+            for notification in notifications_list or []:
+                if notification > last_notification and notification.link != last_notification.link:
+                    channel = self.client.get_channel(misc.getChannelID(self.client, "logger"))
+                    if channel is not None:
+                        await channel.send(notification.link)
+                    lastSent = notification
+
+            if lastSent != last_notification:
+                with open(".\\last_notification_fit_news.json", "w", encoding="utf-8") as jsonDataFile:
+                    json.dump(lastSent.__dict__, jsonDataFile, indent = 4)
             
-                with open(".\\last_notification_fit_news.json", "r", encoding="utf-8") as jsonDataFile:
-                    last_notification_json = json.load(jsonDataFile)
-
-                last_notification = notifications.DLWMS_Notification(
-                    last_notification_json["link"],
-                    last_notification_json["title"],
-                    last_notification_json["date"],
-                    last_notification_json["subject"],
-                    last_notification_json["author"],
-                    last_notification_json["content"]
-                )
-
-                lastSent = last_notification
-                notifications_list.reverse()
-                for notification in notifications_list or []:
-                    if notification > last_notification and notification.link != last_notification.link:
-
-                        channel = self.client.get_channel(misc.getChannelID(self.client, "logger"))
-                        if channel is not None:
-                            await channel.send(notification.link)
-                        lastSent = notification
-
-
-                if lastSent != last_notification:
-                    with open(".\\last_notification_fit_news.json", "w", encoding="utf-8") as jsonDataFile:
-                        json.dump(lastSent.__dict__, jsonDataFile, indent = 4)
-                
-            except Exception as err:
-                print("Error: " + str(err))
+        except Exception as err:
+            print("Error: " + str(err))
             
             
 
     @tasks.loop(minutes = 1)
     async def send_notifications(self):
-        await main()
+        await self.main()
         
     def cog_unload(self):
         self.send_notifications.cancel()
