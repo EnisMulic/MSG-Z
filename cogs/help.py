965b17e6442263fb1c8051be7451fea22a6444e9
Enis Mulić <enis.s.mulic@gmail.com>
2020-09-05 21:43:38 +0200

    Move man command to help

--- cogs/help.py
+++ cogs/help.py
@@ -31,5 +31,17 @@ class Help(commands.Cog):
 
         await ctx.send(embed = helpEmbed)
 
+        @commands.command(aliases=["man"])
+    async def manual(self, ctx, cmd):
+        """Get manual for command."""
+
+        
+        for cog in self.client.cogs:
+            for command in self.client.get_cog(cog).walk_commands():
+                if command.name == cmd or cmd in command.aliases:
+                    cmds_desc = '$[' + ' | '.join(alias for alias in command.aliases) + '] ' + command.signature
+                    cmds_desc = '```\n' + cmds_desc + '```'
+                    await ctx.send(cmds_desc)
+
 def setup(client):
     client.add_cog(Help(client))
