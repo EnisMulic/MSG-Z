1fbdab3d588ac37f9689fc391241b19b407fe0ac
Enis Mulić <enis.s.mulic@gmail.com>
2020-09-07 21:18:51 +0200

    Add lock and unlock commands

--- cogs/admin.py
+++ cogs/admin.py
@@ -32,14 +32,63 @@ class Administrator(commands.Cog):
 
     @commands.command()
     @commands.has_any_role('Administrator')
-    async def lock(self, ctx):
-        everyone = misc.getRoleByName(self.client, "@everyone")
+    async def lock(self, ctx, channel = None):
+        if channel is None:
+            channel = ctx.channel
+        else:
+            channel_id = int(channel[2:len(channel) - 1])
+            channel = self.client.get_channel(channel_id)
+        
+        for role in channel.changed_roles:
+            if role.name != "@everyone":
+                await channel.set_permissions(
+                    role, send_messages = False
+                )
 
-        await ctx.channel.set_permissions(
-            everyone, read_messages=True, 
-                      send_messages=False
-        )
+    @commands.command()
+    @commands.has_any_role('Administrator')
+    async def unlock(self, ctx, channel = None):
+        if channel is None:
+            channel = ctx.channel
+        else:
+            channel_id = int(channel[2:len(channel) - 1])
+            channel = self.client.get_channel(channel_id)
+        
+        for role in channel.changed_roles:
+            if role.name != "@everyone":
+                await channel.set_permissions(
+                    role, send_messages = True
+                )
 
+    @commands.command(aliases=["full-lock", "f-lock"])
+    @commands.has_any_role('Administrator')
+    async def full_lock(self, ctx, channel = None):
+        if channel is None:
+            channel = ctx.channel
+        else:
+            channel_id = int(channel[2:len(channel) - 1])
+            channel = self.client.get_channel(channel_id)
+        
+        for role in channel.changed_roles:
+            await channel.set_permissions(
+                role, read_messages = False
+            )
+                  
+
+    @commands.command(aliases=["full-unlock", "f-unlock"])
+    @commands.has_any_role('Administrator')
+    async def full_unlock(self, ctx, channel = None):
+        if channel is None:
+            channel = ctx.channel
+        else:
+            channel_id = int(channel[2:len(channel) - 1])
+            channel = self.client.get_channel(channel_id)
+        
+        for role in channel.changed_roles:
+            if role.name != "@everyone":
+                await channel.set_permissions(
+                    role, read_messages = True
+                )    
 
 def setup(client):
     client.add_cog(Administrator(client))
