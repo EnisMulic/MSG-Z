b4a0cb3d9b6d4670279a7999bdc1eb2bbb3c7536
Enis Mulić <enis.s.mulic@gmail.com>
2020-11-22 22:13:01 +0100

    Fix color cog

--- cogs/color.py
+++ cogs/color.py
@@ -2,46 +2,41 @@ import discord
 from discord.ext import commands
 from discord.ext import tasks
 
-import asyncio
-import aiohttp
-
 import random
 
 class Color(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command()
+    @commands.command(aliases=["clr"])
     async def color(self, ctx, hex):
         """Get random color."""
-        async with aiohttp.ClientSession() as session:
-            async with session.get('https://www.thecolorapi.com/id?hex={hex}') as response:
+        hex = hex.strip("#")
+        url = f"https://dummyimage.com/900x900/{hex}/{hex}"
+        
+        embed = discord.Embed(
+            title = "Color",
+            description = "#" + hex
+        )  
+        
 
-                embed = discord.Embed(
-                    title = "Color",
-                    description = hex
-                )   
+        embed.set_thumbnail(url = url)
+        await ctx.send(embed = embed)
 
-                data = await response.json()
-                embed.set_thumbnail(url = "http://www.thecolorapi.com/id?format=svg&hex=HEX%7D")
-                await ctx.send(embed = embed)
-
-    @commands.command()
+    @commands.command(aliases=["randclr"])
     async def rand_color(self, ctx):
         r = lambda: random.randint(0,255)
-        hex = '#{:02x}{:02x}{:02x}'.format(r(), r(), r())
+        hex = '{:02x}{:02x}{:02x}'.format(r(), r(), r())
+        url = f"https://dummyimage.com/900x900/{hex}/{hex}"
+        
+        embed = discord.Embed(
+            title = "Color",
+            description = "#" + hex
+        )  
 
-        async with aiohttp.ClientSession() as session:
-            async with session.get('https://www.thecolorapi.com/id?hex={hex}') as response:
-
-                embed = discord.Embed(
-                    title = "Color",
-                    description = hex
-                )   
-
-                data = await response.json()
-                embed.set_thumbnail(url = "http://www.thecolorapi.com/id?format=svg&hex=HEX%7D")
-                await ctx.send(embed = embed)
+        embed.set_thumbnail(url = url)
+        await ctx.send(embed = embed)
+                
 
 def setup(client):
     client.add_cog(Color(client))
