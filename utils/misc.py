48ada98d25297ee73fdacf3ccb0ba2ce9be45c72
Enis Mulić <enis.s.mulic@gmail.com>
2021-06-05 13:45:10 +0200

    rename util methods

--- utils/misc.py
+++ utils/misc.py
@@ -1,31 +1,23 @@
 import discord
 
-def getChannelID(client, channelName):
+def get_channel_id(client, channelName):
     for guild in client.guilds:
         for channel in guild.channels:
             if channel.name == channelName:
                 return channel.id
 
-def getMember(client, member_id):
+def get_member(client, member_id):
     for guild in client.guilds:
-        for member in guild.members:
-            if member.id == member_id:
-                return member
+        return guild.get_member(member_id)
 
-def getRoleByName(client, roleName):
+def get_role_by_name(client, roleName):
     for guild in client.guilds:
         for role in guild.roles:
             if role.name == roleName:
                 return role
 
-def getRoleById(client, roleId):
+def get_role_by_id(client, roleId):
     for guild in client.guilds:
         for role in guild.roles:
             if role.id == roleId:
                 return role
-
-def getDefaultRole(client):
-    print(client.guilds)
-    for guild in client.guilds:
-        print(guild.default_role)
-    return None
