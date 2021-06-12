22e20fb3a0ea1835ea1dc570dda813c7d2a2249c
Enis Mulić <enis.s.mulic@gmail.com>
2021-06-12 16:57:03 +0200

    add env file

--- bot.py
+++ bot.py
@@ -1,20 +1,22 @@
 import discord
 from discord.ext import commands
 
+import os
+from dotenv import load_dotenv
+
 import json
 
-def get_token():
-    with open("config.json", encoding='utf-8') as json_data_file:
-        data = json.load(json_data_file)
-    return data["Discord"]["Token"]
-
 def main():
+    load_dotenv()
+
+    
+
     intents = discord.Intents.all()
     bot = commands.Bot(command_prefix = '$', intents = intents)
 
-    token = get_token()
+    DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN")
 
-    bot.run(token)
+    bot.run(DISCORD_TOKEN)
 
 if __name__ == '__main__':
     main()
