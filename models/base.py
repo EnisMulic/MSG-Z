bbc4721e567ba561417db657000607d132a508ab
Enis Mulić <enis.s.mulic@gmail.com>
2021-06-19 17:48:12 +0200

    setup database

---
+++ models/base.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+import os
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL, max_overflow = -1)
+Session = sessionmaker(bind = engine)
+
+Base = declarative_base()
