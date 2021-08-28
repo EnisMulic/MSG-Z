ee7be5c72fe1dbf28f3f7164dc767670aaa30cf2
Enis Mulić <enis.s.mulic@gmail.com>
2021-08-28 18:31:36 +0200

    Add special invite command for administrators

--- cogs/misc.py
+++ cogs/misc.py
@@ -16,7 +16,26 @@ class Misc(commands.Cog):
 
         channel_lobby = discord.utils.get(ctx.guild.text_channels, name=channels.LOBBY)
         
-        invite_link = await channel_lobby.create_invite(max_uses = 1, max_age = 48 * 60 * 60, unique = True, reason = f"Created by {ctx.author.display_name}")
+        invite_link = await channel_lobby.create_invite(max_uses = 1,
+                                                        max_age = 48 * 60 * 60,
+                                                        unique = True,
+                                                        reason = f"Created by {ctx.author.display_name}")
+
+        channel_dm = await ctx.author.create_dm()
+        await channel_dm.send(invite_link)
+    
+    @commands.command()
+    @commands.command(aliases=["inv"])
+    @commands.has_permissions(administrator = True)
+    async def invite_(self, ctx, max_uses = 0, max_age = 0):
+        """Create a one time use invite"""
+
+        channel_lobby = discord.utils.get(ctx.guild.text_channels, name=channels.LOBBY)
+        
+        invite_link = await channel_lobby.create_invite(max_age = max_age,
+                                                        max_uses = max_uses,
+                                                        unique = True,
+                                                        reason = f"Created by {ctx.author.display_name}")
 
         channel_dm = await ctx.author.create_dm()
         await channel_dm.send(invite_link)
